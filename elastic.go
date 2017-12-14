c5542ea7254a57a50d47da1c3fc994330a775aa9
Stefan Majer <stefan.majer@f-i-ts.de>
2017-12-14 15:03:30 +0100

    createIndex is not needed

--- elastic.go
+++ elastic.go
@@ -1,32 +1,10 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/google/uuid"
 	"github.com/olivere/elastic"
 	log "github.com/sirupsen/logrus"
 )
-
-func (r *redisClient) createIndex(name string) error {
-	exists, err := r.ec.IndexExists(name).Do(context.Background())
-	if err != nil {
-		return fmt.Errorf("index:%s cannot be checked:%v", name, err)
-	}
-	if exists {
-		return nil
-	}
-	createIndex, err := r.ec.CreateIndex(name).Do(context.Background())
-	if err != nil {
-		return fmt.Errorf("cannot create index:%s %v", name, err)
-	}
-	if !createIndex.Acknowledged {
-		return fmt.Errorf("create index:%s was not acknowledged", name)
-	}
-	log.WithFields(log.Fields{"created index:": name}).Debug("index:")
-	return nil
-}
 
 func (r *redisClient) index(documents chan document) {
 	for {
